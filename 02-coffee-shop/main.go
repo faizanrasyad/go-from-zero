27ed0e590ad141231ff0c75a0c9e0521f4272a24
faizanrasyad <rasyadfaizan@gmail.com>
2026-07-23 09:17:20 +0700

    Main Class

---
+++ 02-coffee-shop/main.go
@@ -0,0 +1,59 @@
+package coffeeshop
+
+import "fmt"
+
+type MenuItem struct {
+	ID int
+	Name string
+	Price float64
+}
+
+type Order struct {
+	OrderID string
+	CustomerName string
+	Items []OrderItem
+}
+
+type OrderItem struct {
+	Menu MenuItem
+	Quantity int
+}
+
+var Menus = []MenuItem {
+	{ID: 1, Name: "Espresso", Price: 25000},
+	{2, "Latte", 35000},
+	{3, "Cappuccino", 32000},
+	{4, "Americano", 28000},
+	{5, "Mocha", 38000},
+	{6, "Matcha Latte", 40000},
+	{7, "Croissant", 22000},
+	{8, "Cheesecake", 30000},
+}
+
+func main() {
+	fmt.Println("===========================")
+	fmt.Println("BrewHub Coffee Cashier")
+	fmt.Println("===========================")
+	fmt.Println("")
+	fmt.Println("1. Show Menu")
+	fmt.Println("2. Create Order")
+	fmt.Println("3. View Orders")
+	fmt.Println("4. Search Order")
+	fmt.Println("5. Cancel Order")
+	fmt.Println("6. Daily Sales")
+	fmt.Println("7. Exit")
+	fmt.Print("Choose: ")
+	
+	var chosen int
+	fmt.Scanln(&chosen)
+
+	switch chosen {
+	case 1: 
+	case 2:
+	case 3:
+	case 4:
+	case 5:
+	case 6:
+	case 7:
+	}
+}
